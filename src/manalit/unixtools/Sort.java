e9955a9412293e1e1b8e1cd08876a4843e1a68ab
Manali <manu@RLB.com>
2014-01-15 10:26:55 +0530

    sort input handling done

--- src/manalit/unixtools/Sort.java
+++ src/manalit/unixtools/Sort.java
@@ -1,25 +1,40 @@
 package manalit.unixtools;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 
 public class Sort {
-    public static void main(String args[])throws IOException {
-        String sortedDAta;
-        String reverseDAta;
-        String text;
-        ReadFile readFile = new ReadFile();
-        text =readFile.read(args[0]);
+    public static void main(String args[]) throws IOException {
+        Sort sortclient = new Sort();
+        ReadFile fs = new ReadFile();
         SortLib sort = new SortLib();
-        if(args.length == 2 && (0 == args[1].compareTo("-r"))){
-            reverseDAta = sort.reverseData(text);
-            System.out.println(reverseDAta);
+        String columnData;
+        String properArgv[] = sortclient.getArguments(args);
+        String fileData = fs.read(properArgv[0]);
+        if (properArgv[1] == null)
+            columnData = sort.sortData(fileData);
+        else
+            columnData = sort.reverseData(fileData);
+        System.out.println(columnData);
+    }
+
+    String[] getArguments(String[] arg) {
+        String options[] = new String[2];
+        for (int i = 0; i < arg.length; i++) {
+            if (Sort.isReverse(arg[i]))
+                options[1] = arg[i];
+            if (!Sort.isFile(arg[i]))
+                options[0] = arg[i];
         }
-        else {
-            sortedDAta = sort.sortData(text);
-            System.out.println(sortedDAta);
-        }
+        return options;
+    }
+
+    static boolean isReverse(String arg) {
+        return arg.matches("-r.*");
+    }
+
+    static boolean isFile(String arg) {
+        return arg.matches("-.*");
     }
 }
+
 
